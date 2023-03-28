977fc2d56b7eb916a6cb2da2c40503a37e0a8128
katiehub123 <hkhe@confederationcollege.ca>
2023-03-28 15:45:25 -0400

    add items to list, remove items when need

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
+<script>
+  import Todo from "./Todo.svelte";
+</script>
+
 <h1>My Todo App</h1>
+<Todo />
+
+<style>
+     h1{
+          color: rgb(221, 101, 101);
+          font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+          text-align:center ;
+     }
+</style>
---
+++ src/routes/Todo.svelte
@@ -0,0 +1,84 @@
+<script>
+     let todoItem = ''
+     let todoList = []
+
+     function addThis(){
+          if (todoItem == ''){
+               return;
+          }
+          todoList = [...todoList,{
+               text: todoItem,
+               done: false
+          }]
+          todoItem = "";
+     }
+     function removeThis(index){
+          todoList.splice(index, 1)
+          todoList = todoList;
+     }
+     function removeDone(){
+          todoList = todoList.filter (t => !t.done)
+     }
+     function removeAll(){
+          todoList = [];
+     }
+
+</script>
+
+<div class="main">
+     <form on:submit|preventDefault={addThis}>
+          <input bind:value={todoItem} type="text" autofocus>
+          <button type="submit"> Add</button>
+     </form>
+     
+     <ul>
+          {#each todoList as item, index}
+               <li>
+                    <input type="checkbox" bind:checked={item.done}>
+                    <span class:done={item.done}>{item.text}</span>
+                    <span on:click={() => removeThis(index)} on:keypress={() => removeThis(index)} class="remove"></span>
+               </li>
+          {/each}
+     </ul>
+     <button on:click={removeDone}> Remove Done</button>
+     <button on:click={removeAll}> Remove All</button>
+
+</div>
+
+
+<style>
+     .main{
+          margin: auto;
+          padding: 2rem;
+          display: block;
+          width: 50vw;
+          font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+     }
+     form{
+          text-align: center;
+     }
+     li{
+          list-style: none;
+          text-align: center;
+          font-size: 1.7rem;
+     }
+     button{
+          border-radius: 2rem;
+          border: rgb(255, 185, 167);
+          background-color: rgb(255, 242, 167);
+          color: rgb(221, 101, 101);
+          font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+     }
+     .done{
+          text-decoration: line-through;
+          color: rgb(221, 101, 101);
+     }
+     .remove{
+          height: 1.7rem;
+          width: 1.7rem;
+          display: inline-block;
+          background: url('./images/trashbin.png') no-repeat;
+          background-size: 100% auto;
+          cursor: pointer;
+     }
+</style>
